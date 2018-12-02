e560374343380f04e0ede170689e105b5fb4f40e
Kyle Chilcutt <kyle@chilcutt.com>
2018-12-02 11:20:24 -0800

    Add function to fetch PR info from GitHub API
    Use the args from the console to fetch the PR info for a single repo
    from the API.

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,35 @@
 #!/usr/bin/env node
 
+const fetch = require('node-fetch');
 const minimist = require('minimist');
+
 const args = minimist(process.argv.slice(2), {
   alias: {
+    r: 'repo',
     t: 'token',
   },
-  string: ['token'],
+  string: [
+    'token',
+    'repo',
+  ],
 });
 
-console.log(args);
+async function perform() {
+  const prData = await getPullRequests(args);
+  console.log(prData);
+}
+
+perform();
+
+async function getPullRequests({ token, repo }) {
+  return fetch(
+    `https://api.github.com/repos/${repo}/pulls`,
+    {
+      headers: {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': `token ${token}`,
+        'Content-Type': 'application/json',
+      },
+    },
+  ).then(response => response.json());
+}
